978568e6c216756ff7c221ece69616bac323838d
Wei <weihsu@inquartik.com>
2021-08-30 03:54:24 +0800

    feat: add CRUD API format answer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ function App() {
   return (
     <div className={styles.app}>
       <div className={styles.app__answer}>
+        <h1 className={styles.app__answerTitle}>
+          請持續往下捲動，題目及回答分別為<br />
+          Q1. Layout1, Layout2<br />
+          Q2. 多選清單<br />
+          Q3. 前端常用到的 CRUD API 規格<br />
+        </h1>
+      </div>
+      <div className={styles.app__answer}>
         <h1 className={styles.app__answerTitle}>Layout1</h1>
         <Layout1 />
       </div>
@@ -18,6 +26,15 @@ function App() {
       <div className={styles.app__answer}>
         <h1 className={styles.app__answerTitle}>多選清單</h1>
         <MultipleCheckboxList />
+      </div>
+      <div className={styles.app__answer}>
+        <h1 className={styles.app__answerTitle}>前端常用到的 CRUD API 規格</h1>
+        <a
+          target="_blank"
+          href="https://www.evernote.com/shard/s456/sh/630c1a8d-87de-6e1e-ed2c-a275f672a174/b5fedc9daf829bef3d27ebcec221b000" rel="noreferrer"
+        >
+          前端常用到的 CRUD API 規格
+        </a>
       </div>
     </div>
   );
